f88ee9b8b552373154c8c2b2e2576a79eaa7cf95
zombieseatflesh7 <lilmanjoshua@gmail.com>
2025-10-18 18:45:19 -0500

    Initial commit

---
+++ src/Plugin.cs
@@ -0,0 +1,24 @@
+﻿using BepInEx;
+using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using System;
+using System.Security.Permissions;
+using UnityEngine;
+
+#pragma warning disable CS0618
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+#pragma warning restore CS0618
+
+namespace MenuFixes;
+
+[BepInPlugin("zombieseatflesh7.MenuFixes", "Menu Fixes", "1.0.0")]
+sealed class Plugin : BaseUnityPlugin
+{
+    public static new BepInEx.Logging.ManualLogSource Logger;
+
+    public void OnEnable()
+    {
+        Logger = base.Logger;
+
+    }
+}
---
+++ src/ScrollFix.cs
@@ -0,0 +1,55 @@
+﻿using Mono.Cecil.Cil;
+using MonoMod.Cil;
+using System;
+using UnityEngine;
+
+namespace MenuFixes
+{
+    public static class ScrollFix
+    {
+        public static void AddHooks()
+        {
+            try
+            {
+                IL.Menu.Menu.Update += Menu_UpdateIL;
+                On.MainLoopProcess.RawUpdate += MainLoopProcess_RawUpdate;
+            }
+            catch (Exception e)
+            {
+                Plugin.Logger.LogError("Failed to load Scroll Fix");
+                Plugin.Logger.LogError(e.Message + "\n" + e.StackTrace);
+            }
+        }
+
+        private static void MainLoopProcess_RawUpdate(On.MainLoopProcess.orig_RawUpdate orig, MainLoopProcess self, float dt)
+        {
+            // update scroll delta
+            if (self.manager.currentMainLoop == self && self.manager.menuesMouseMode)
+            {
+                if (self is Menu.Menu)
+                    (self as Menu.Menu).floatScrollWheel += Input.GetAxis("Mouse ScrollWheel");
+                else if (self is RainWorldGame && (self as RainWorldGame).pauseMenu != null)
+                    (self as RainWorldGame).pauseMenu.floatScrollWheel += Input.GetAxis("Mouse ScrollWheel");
+            }
+            orig(self, dt);
+        }
+
+        private static void Menu_UpdateIL(ILContext il)
+        {
+            ILCursor c = new(il);
+
+            // remove scroll delta update, because it is handled in RawUpdate now
+            c.GotoNext(
+                i => i.MatchLdarg(0),
+                i => i.MatchLdarg(0),
+                i => i.MatchLdfld("Menu.Menu", "floatScrollWheel")
+                );
+            c.RemoveRange(7);
+
+            // number edit from 15f to 10f
+            c.GotoNext(i => i.MatchLdcR4(15f));
+            c.Remove();
+            c.Emit(OpCodes.Ldc_R4, 10f);
+        }
+    }
+}
