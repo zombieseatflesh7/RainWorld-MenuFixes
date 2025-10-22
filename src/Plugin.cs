da37fc90590d370cd14bc257cac35d8a37958a67
zombieseatflesh7 <lilmanjoshua@gmail.com>
2025-10-22 02:09:23 -0500

    Significant remix menu optimizations
    fixed a massive memory leak and greatly reduced memory usage and load time of the remix menu.
    It still stutters though.

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -5,6 +5,7 @@ using MonoMod.Cil;
 using MonoMod.RuntimeDetour;
 using System;
 using System.Security.Permissions;
+using UnityEngine;
 
 #pragma warning disable CS0618
 [assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
@@ -16,20 +17,37 @@ namespace MenuFixes;
 public class Plugin : BaseUnityPlugin
 {
     public static new BepInEx.Logging.ManualLogSource Logger;
+    private static bool initialized = false;
 
     public void OnEnable()
     {
         Logger = base.Logger;
+
         On.RainWorld.OnModsInit += OnModsInit;
     }
 
+    // most of this code breaks if its run OnEnable for some reason lmao
     private void OnModsInit(On.RainWorld.orig_OnModsInit orig, RainWorld self)
     {
-        // TODO: compatibility check with existing mods that do the same thing
-
-        ScrollFix.AddHooks();
-        ThumbnailFix.AddHooks(); // This hook crashes the game if its applied OnEnable
-
         orig(self);
+
+        if (!initialized)
+        {
+            initialized = true;
+            try
+            {
+                AssetBundle assetBundle = AssetBundle.LoadFromFile(AssetManager.ResolveFilePath("menufixesbundle"));
+                self.Shaders.Add("MenuFixes.Greyscale", FShader.CreateShader("MenuFixes.Greyscale", assetBundle.LoadAsset<Shader>("Assets/Shaders/Greyscale.shader")));
+
+                // TODO: compatibility check with existing mods that do the same thing
+
+                ScrollFix.AddHooks();
+                OptimizedRemix.AddHooks();
+            }
+            catch (Exception ex)
+            {
+                Debug.LogException(ex);
+            }
+        }
     }
 }
